b5292afc1688ba72522396dbd9f390f8030fa4d2
kryozor <shawnjb.contact@gmail.com>
2023-04-17 20:36:24 -0700

    Update logger.lua

--- logger.lua
+++ logger.lua
@@ -1,36 +1,153 @@
-require("longod.DPSTooltips.test").new().Run(false)
+local this = {}
+this.name = "DPS Tooltips"
+local config = require("longod.DPSTooltips.config")
 
-local config = require("longod.DPSTooltips.config").Load()
-local dps = require("longod.DPSTooltips.dps").new(config)
-local drawer = require("longod.DPSTooltips.drawer").new(config)
+function this.OnModConfigReady()
+    local data = config.Load()
 
---- Checks if the provided object is a weapon or ammunition.
---- @param object tes3physicalObject The object to check.
---- @return boolean @Returns true if the object is a weapon or ammunition, false otherwise.
-local function IsWeapon(object)
-    if object and (object.objectType == tes3.objectType.weapon or object.objectType == tes3.objectType.ammunition) then
-        return true
+    local template = mwse.mcm.createTemplate(this.name)
+    template:saveOnClose(config.configPath, data)
+    template:register()
+
+    local page = template:createSideBarPage {
+        label = "Settings",
+        description = (
+            "This mod analytically calculates weapon DPS and displays it in weapon tooltips.\n" ..
+            "You can know which weapons are actually stronger for your player character."
+            )
+    }
+
+    page:createOnOffButton {
+        label = "Enable DPS Tooltip",
+        description = (
+            "Enable this tooltip feature.\n" ..
+            "\nDefault: On"
+            ),
+        variable = mwse.mcm.createTableVariable {
+            id = "enable",
+            table = data,
+        }
+    }
+
+    page:createOnOffButton {
+        label = "Display Min - Max",
+        description = (
+            "Show minimum to maximum DPS range. When disabled, only display maximum.\n" ..
+            "In Morrowind, the weapon's damage range is determined by how long the attack key is held, not by RNG. Therefore, the average value does not become DPS.\n" ..
+            "\nDefault: On"
+            ),
+        variable = mwse.mcm.createTableVariable {
+            id = "minmaxRange",
+            table = data,
+        }
+    }
+
+    page:createOnOffButton {
+        label = "Insert Pre-Divider",
+        description = (
+            "Insert a dividing line BEFORE the DPS display. Makes it easier to distinguish when using other tooltips mods.\n" ..
+            "\nDefault: Off"
+            ),
+        variable = mwse.mcm.createTableVariable {
+            id = "preDivider",
+            table = data,
+        }
+    }
+
+    page:createOnOffButton {
+        label = "Insert Post-Divider",
+        description = (
+            "Insert a dividing line AFTER the DPS display. Makes it easier to distinguish when using other tooltips mods.\n" ..
+            "\nDefault: Off"
+            ),
+        variable = mwse.mcm.createTableVariable {
+            id = "postDivider",
+            table = data,
+        }
+    }
+
+    do
+        local sub = page:createCategory("Accurate DPS")
+        sub:createOnOffButton {
+            label = "Use accurate weapon damage",
+            description = (
+                "Use accurate weapon damage dealt, taking into account the player character's attributes and the weapon condition.\n" ..
+                "\nDefault: On"
+                ),
+            variable = mwse.mcm.createTableVariable {
+                id = "accurateDamage",
+                table = data,
+            }
+        }
+        sub:createOnOffButton {
+            label = "Use best weapon condition",
+            description = (
+                "Always determine DPS as the weapon with the best durability. This is useful when you want to consider theoretical values.\n" ..
+                "\nDefault: On"
+                ),
+            variable = mwse.mcm.createTableVariable {
+                id = "maxDurability",
+                table = data,
+            }
+        }
     end
-    return false
-end
 
---- Event callback function for displaying DPS information in weapon tooltips.
---- @param e uiObjectTooltipEventData The event data.
-local function OnUiObjectTooltip(e)
-    if config.enable and IsWeapon(e.object) then
-        local useBestAttack = tes3.worldController.useBestAttack
-        local object = e.object ---@cast object tes3weapon
-        local data = dps:CalculateDPS(object, e.itemData, useBestAttack)
-        drawer:Display(e.tooltip, data, useBestAttack)
+    do
+        local sub = page:createCategory("Breakdown Appearance")
+        sub:createOnOffButton {
+            label = "Display DPS breakdown",
+            description = (
+                "You can know the difference in damage for each weapon swing type, and damages caused by enchantments.\n" ..
+                "\nDefault: On"
+                ),
+            variable = mwse.mcm.createTableVariable {
+                id = "breakdown",
+                table = data,
+            }
+        }
+        sub:createOnOffButton {
+            label = "Coloring text",
+            description = (
+                "For each damage, add color to text by elemental or school.\n" ..
+                "\nDefault: On"
+                ),
+            variable = mwse.mcm.createTableVariable {
+                id = "coloring",
+                table = data,
+            }
+        }
+        sub:createOnOffButton {
+            label = "Display effect icons",
+            description = (
+                "For each damage, display enchantment icons that affected it. For example, it makes it easier to see what the weakness spell has affected.\n" ..
+                "\nDefault: On"
+                ),
+            variable = mwse.mcm.createTableVariable {
+                id = "showIcon",
+                table = data,
+            }
+        }
     end
+    page:createDropdown {
+        label = "Logging Level",
+        description = (
+            "Set the log level.\n" .. "\nDefault: INFO"
+            ),
+        options = {
+            { label = "TRACE", value = "TRACE" },
+            { label = "DEBUG", value = "DEBUG" },
+            { label = "INFO",  value = "INFO" },
+            { label = "WARN",  value = "WARN" },
+            { label = "ERROR", value = "ERROR" },
+            { label = "NONE",  value = "NONE" },
+        },
+        variable = mwse.mcm.createTableVariable { id = "logLevel", table = data },
+        callback = function(self)
+            local logger = require("longod.DPSTooltips.logger")
+            ---@diagnostic disable: need-check-nil
+            logger:setLogLevel(self.variable.value)
+        end
+    }
 end
 
---- Initializes the DPS Tooltips mod.
-local function OnInitialized()
-    dps:Initialize()
-    drawer:Initialize()
-    event.register(tes3.event.uiObjectTooltip, OnUiObjectTooltip, { priority = 0 }) -- TODO tweaks priority
-end
-
-event.register(tes3.event.initialized, OnInitialized)
-event.register(tes3.event.modConfigReady, require("longod.DPSTooltips.mcm").OnModConfigReady)
+return this
