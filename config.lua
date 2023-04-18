a4410748b59539d4ee7a4feaa055eadd4187d77e
kryozor <shawnjb.contact@gmail.com>
2023-04-17 20:00:43 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,19 @@
+--- @module '"longod.DPSTooltips.config"'
+
+--- @type table
 local this = {}
 
----@class Config
+--- @class Config: table
+--- @field enable boolean Enable the mod.
+--- @field minmaxRange boolean Show the minimum and maximum range of the weapon.
+--- @field preDivider boolean Show the pre-divider damage value.
+--- @field postDivider boolean Show the post-divider damage value.
+--- @field accurateDamage boolean Show the accurate damage value.
+--- @field maxDurability boolean Show the maximum durability of the weapon.
+--- @field breakdown boolean Show the damage breakdown.
+--- @field coloring boolean Enable coloring of the tooltip.
+--- @field showIcon boolean Show the icon in the tooltip.
+--- @field logLevel string The log level to use. One of "DEBUG", "INFO", "WARN", "ERROR", or "FATAL".
 this.defaultConfig = {
     enable = true,
     minmaxRange = true,
@@ -8,27 +21,26 @@ this.defaultConfig = {
     postDivider = false,
     accurateDamage = true,
     maxDurability = true,
-    -- maxFatigue = true,
     breakdown = true,
-    -- always or pressed key
-    -- hitRate = false,
-    -- armor = false,
-    -- blocking = false,
-    -- difficulty = false,
     coloring = true,
     showIcon = true,
     logLevel = "INFO",
 }
-this.config = nil ---@type Config
+
+--- @type Config
+this.config = nil
+--- @type string
 this.configPath = "longod.DPSTooltips"
 
----@return Config
+--- Loads the configuration for the DPS module from disk or returns the cached configuration if it has already been loaded.
+--- @return Config @The configuration object for the DPS module.
 function this.Load()
     this.config = this.config or mwse.loadConfig(this.configPath, this.defaultConfig)
     return this.config
 end
 
----@return Config
+--- Returns the default configuration table.
+--- @return Config @The default configuration table.
 function this.Default()
     return table.deepcopy(this.defaultConfig)
 end
